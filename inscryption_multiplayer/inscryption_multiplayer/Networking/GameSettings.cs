50d7e1d203917bcd6ae3802bed42b542610943ef
Qkrisi <qkrisiyt@gmail.com>
2023-09-27 16:32:47 +0200

    New settings (UI)

--- inscryption_multiplayer/inscryption_multiplayer/Networking/GameSettings.cs
+++ inscryption_multiplayer/inscryption_multiplayer/Networking/GameSettings.cs
@@ -15,5 +15,12 @@ namespace inscryption_multiplayer.Networking
         public LobbyAccess LobbyType = LobbyAccess.FriendsOnly;
         public int MapsUsed = 1;
         public bool AllowTotems;
+        public bool AllowItems = true;
+        public int ScaleSize = 5;
+        public int NodeWidth = 3;
+        public int NodeLength = 13;
+        public bool AllowBackrows = true;
+        public bool AllowSacrificeOnFrontrows;
+        [JsonIgnore] public bool PreviousAllowSecrificeOnFrontrows;
     }
 }
