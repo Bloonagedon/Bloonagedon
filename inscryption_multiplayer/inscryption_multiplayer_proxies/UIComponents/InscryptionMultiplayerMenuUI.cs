f64db5acc00e234edaad71ee8f8a80d4ec97f895
Lily <56913305+Memez4Life7@users.noreply.github.com>
2023-09-30 23:02:04 +0200

    Revert "Merge branch 'main' of https://github.com/Memez4Life7/Inscryption-Multiplayer"
    This reverts commit 35668f2b40d7fba6c469f483aacbfc2e094a7ba9, reversing
    changes made to e2f4c2994cd4ddb7672b968b2645d8f84a9d35a2.

--- inscryption_multiplayer/inscryption_multiplayer_proxies/UIComponents/InscryptionMultiplayerMenuUI.cs
+++ inscryption_multiplayer/inscryption_multiplayer_proxies/UIComponents/InscryptionMultiplayerMenuUI.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,65 +17,17 @@ public class InscryptionMultiplayerMenuUI : MonoBehaviour
     
     #region Settings
     public GameObject TabGroup_Settings;
-    public GameObject[] SettingsPages;
     public GenericUIButtonProxy SettingsStartButton;
     public GenericUIButtonProxy LobbyAccessButton;
     public Text LobbyAccessText;
     public Text SaveSettingsText;
-    public GenericUIButtonProxy PreviousPageButton;
-    public GenericUIButtonProxy NextPageButton;
-    public Text PageText;
-    public int CurrentPage { get; private set; }
 
-    //Page 1
     public GenericUIButtonProxy MapsPlusButton;
     public GenericUIButtonProxy MapsMinusButton;
     public Text MapsNumberText;
+
     public GenericUIButtonProxy ToggleTotemsButton;
     public Text TotemSettingText;
-    public GenericUIButtonProxy ToggleItemsButton;
-    public Text ItemSettingText;
-    //
-    
-    //Page 2
-    public GenericUIButtonProxy ToggleBackrowsButton;
-    public Text BackrowsSettingText;
-    public GenericUIButtonProxy ToggleFrontrowSacrificesButton;
-    public Text SacrificeSettingText;
-    //
-    
-    //Page 3
-    public GenericUIButtonProxy ScaleSizeMinusButton;
-    public GenericUIButtonProxy ScaleSizePlusButton;
-    public Text ScaleSizeText;
-    public GenericUIButtonProxy NodeWidthMinusButton;
-    public GenericUIButtonProxy NodeWidthPlusButton;
-    public Text NodeWidthText;
-    public GenericUIButtonProxy NodeLengthMinusButton;
-    public GenericUIButtonProxy NodeLengthPlusButton;
-    public Text NodeLengthText;
-    //
-
-    public void AdvanceSettingsPage(int offset)
-    {
-        var newIndex = Mathf.Max(Mathf.Min(CurrentPage + offset, SettingsPages.Length - 1), 0);
-        if (newIndex != CurrentPage)
-        {
-            SettingsPages[CurrentPage].SetActive(false);
-            SettingsPages[newIndex].SetActive(true);
-            CurrentPage = newIndex;
-            PageText.text = $"PAGE {newIndex+1}/{SettingsPages.Length}";
-        }
-    }
-
-    public void ResetSettingsPages()
-    {
-        foreach(var page in SettingsPages.Skip(1))
-            page.SetActive(false);
-        SettingsPages[0].SetActive(true);
-        CurrentPage = 0;
-        PageText.text = $"PAGE 1/{SettingsPages.Length}";
-    }
     #endregion
     
     #region LobbyTab
@@ -96,6 +46,5 @@ public class InscryptionMultiplayerMenuUI : MonoBehaviour
         TabGroup_Settings.SetActive(false);
         TabGroup_Lobby.SetActive(false);
         TabGroup_JoinMode.SetActive(true);
-        ResetSettingsPages();
     }
 }
